115c31037f2c1fcd7a147675e1c9ace563bd5794
hyerin-kang <hkang0420@gmail.com>
2025-03-12 16:13:46 +0900

    #1 : 라우팅 구성

---
+++ app/champions/[id]/DetailPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DetailPage = () => {
+  return <div>챔피언상세페이지</div>;
+};
+
+export default DetailPage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,6 +29,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header className="bg-gray-800 text-white py-4 w-full ">
+          <nav className="container mx-auto flex justify-around">
+            <Link href="/">홈</Link>
+            <Link href="/champions">챔피언 목록</Link>
+            <Link href="/items">아이템 목록</Link>
+            <Link href="/rotation">챔피언 로테이션</Link>
+          </nav>
+        </header>
         {children}
       </body>
     </html>
